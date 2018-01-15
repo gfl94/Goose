d45e4e0a4b5e14208294c423d2e3f10432ec7246
Cheng Chen <cheng.chen@microsoft.com>
2018-01-15 15:01:15 +0800

    Goose.Scanner

---
+++ Goose.Scanner.Test/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goose.Scanner.Test
+{
+    class Class1
+    {
+        public void M()
+        {
+            var pairs = GooseTypePairs.Build(options =>
+            {
+                //options.FromAssembly(sourceAssembly)
+                //    .ToAssembly(targetAssembly)
+                //    .WithDefaultConvention()     //make all Foo -> IFoo pattern pair
+                //    .WithConvention((sourceType, targetType) => targetType.Name == "IStandard" + sourceType.Name);  // Foo -> IStandardFoo convention
+
+                //other similiar registers
+            });
+
+            //use the pairs (probably cache it)
+        }
+    }
+}
---
+++ Goose.Scanner/GooseTypePairs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Goose.Scanner
+{
+    public class GooseTypePairs
+    {
+        public static GooseTypePair[] Build(Action<BuilderOptions> options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
